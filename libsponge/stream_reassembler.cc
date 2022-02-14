46e08a38b4760c99c0fca9c73ba6b3b52935d8a0
haoliu <8673355+haoliu97@user.noreply.gitee.com>
2022-02-14 17:35:06 +0800

    lab1 finished

--- libsponge/stream_reassembler.cc
+++ libsponge/stream_reassembler.cc
@@ -12,15 +12,133 @@ void DUMMY_CODE(Targs &&... /* unused */) {}
 
 using namespace std;
 
+int StreamReassembler::merge_block(block_node& a, block_node& b) {
+    bool flag = false; // 区分向前合并 向后合并
+    if(a.begin > b.begin) {
+        swap(a, b);
+        flag = true;
+    }
+
+    if(a.begin + a.length < b.begin) { // can't merge
+        if(flag) swap(a, b);
+        return -1;
+    }
+
+    if(a.begin + a.length >= b.begin + b.length) {
+        if(flag) return a.length;
+        return b.length;
+    }
+    
+    a.data = a.data + b.data.substr(a.begin + a.length - b.begin);
+    a.length = a.data.length();
+    return a.begin + a.length - b.begin;
+}
+
 StreamReassembler::StreamReassembler(const size_t capacity) : _output(capacity), _capacity(capacity) {}
 
 //! \details This function accepts a substring (aka a segment) of bytes,
 //! possibly out-of-order, from the logical stream, and assembles any newly
 //! contiguous substrings and writes them into the output stream in order.
-void StreamReassembler::push_substring(const string &data, const size_t index, const bool eof) {
-    DUMMY_CODE(data, index, eof);
+void StreamReassembler::push_substring(const string &data, const uint64_t index, const bool eof) {
+    if(index >= _head_index + _capacity) return; // unable to push
+    if(data.length() == 0 || index + data.length() <= _head_index) {
+        if(eof) {
+            _eof = eof;
+        }
+        if(_eof && empty()) {
+            _output.end_input();
+        }
+        return;
+    }
+
+    block_node node;
+    if(index < _head_index) {
+        node.begin = _head_index;
+        node.data = data.substr(_head_index - index);
+        node.length = node.data.length();
+    } else {
+        node.begin = index;
+        node.data = data;
+        node.length = data.length();
+    }
+
+    if(_blocks.empty()) {
+        _blocks.insert(node);
+        _unassembled_byte = node.length;
+    } else {
+        auto iter = lower_bound(_blocks.begin(), _blocks.end(), node);
+        int merge_bytes;
+        auto iter_next = iter;
+
+        if(iter != _blocks.end()) {
+            block_node next = *iter;
+            // merge next
+            while(iter != _blocks.end() && (merge_bytes = merge_block(node, next)) != -1) {
+                iter_next = iter;
+                iter_next ++;
+                _unassembled_byte -= merge_bytes;
+                _blocks.erase(iter);
+                if(iter_next != _blocks.end()) {
+                    next = *iter_next;
+                    iter = iter_next;
+                } else {
+                    break;
+                }
+            }
+        }
+
+        iter = lower_bound(_blocks.begin(), _blocks.end(), node);
+        // merge prev
+        if(iter != _blocks.begin()) {
+            iter --;
+            auto next = *iter;
+            while((merge_bytes = merge_block(node, next)) != -1) {
+                iter_next = iter;
+                iter_next --;
+                _unassembled_byte -= merge_bytes;
+                if(iter != _blocks.begin()) {
+                    _blocks.erase(iter);
+                    next = *iter_next;
+                    iter = iter_next;
+                } else {
+                    _blocks.erase(iter);
+                    break;
+                }
+            }
+        }
+
+        _blocks.insert(node);
+        _unassembled_byte += node.length;
+    }
+
+    while(!_blocks.empty() && _blocks.begin()->begin == _head_index) {
+        auto iter = _blocks.begin();
+        size_t write_bytes = _output.write(iter->data);
+        _head_index += write_bytes;
+        _unassembled_byte -= write_bytes;
+        if(write_bytes < iter->length) {
+            node = *iter;
+            _blocks.erase(iter);
+            node.length -= write_bytes;
+            node.data = node.data.substr(write_bytes);
+            node.begin = _head_index;
+            break;
+        }
+        _blocks.erase(iter);
+    }
+
+    if(eof) {
+        _eof = eof;
+    }
+    if(_eof && empty()) {
+        _output.end_input();
+    }
 }
 
-size_t StreamReassembler::unassembled_bytes() const { return {}; }
+size_t StreamReassembler::unassembled_bytes() const {
+    return _unassembled_byte;
+}
 
-bool StreamReassembler::empty() const { return {}; }
+bool StreamReassembler::empty() const {
+    return _unassembled_byte == 0;
+}
--- libsponge/stream_reassembler.hh
+++ libsponge/stream_reassembler.hh
@@ -5,16 +5,30 @@
 
 #include <cstdint>
 #include <string>
+#include <set>
 
 //! \brief A class that assembles a series of excerpts from a byte stream (possibly out of order,
 //! possibly overlapping) into an in-order byte stream.
 class StreamReassembler {
   private:
     // Your code here -- add private members as necessary.
+    struct block_node {
+      size_t begin = 0;
+      size_t length = 0;
+      std::string data = "";
+      bool operator<(const block_node& t) const {
+        return begin < t.begin;
+      }
+    };
 
+    std::set<block_node> _blocks = {};
+    size_t _head_index = 0;
+    size_t _unassembled_byte = 0;
+    bool _eof = false;
     ByteStream _output;  //!< The reassembled in-order byte stream
     size_t _capacity;    //!< The maximum number of bytes
 
+    int merge_block(block_node& a, block_node& b);
   public:
     //! \brief Construct a `StreamReassembler` that will store up to `capacity` bytes.
     //! \note This capacity limits both the bytes that have been reassembled,
